610aa88b88fd732d11bc5b74e1bf2a572e601074
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-04-07 16:38:25 +0700

    add login form

---
+++ QuanLySach/Login.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace QuanLySach
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel21 = new System.Windows.Forms.Panel();
+            this.cbChiNhanh = new System.Windows.Forms.ComboBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLoginName = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLocHD = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel21.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel21
+            // 
+            this.panel21.Controls.Add(this.cbChiNhanh);
+            this.panel21.Controls.Add(this.label18);
+            this.panel21.Location = new System.Drawing.Point(21, 50);
+            this.panel21.Name = "panel21";
+            this.panel21.Size = new System.Drawing.Size(385, 39);
+            this.panel21.TabIndex = 3;
+            // 
+            // cbChiNhanh
+            // 
+            this.cbChiNhanh.FormattingEnabled = true;
+            this.cbChiNhanh.Location = new System.Drawing.Point(131, 5);
+            this.cbChiNhanh.Name = "cbChiNhanh";
+            this.cbChiNhanh.Size = new System.Drawing.Size(251, 21);
+            this.cbChiNhanh.TabIndex = 1;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(10, 9);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(58, 13);
+            this.label18.TabIndex = 0;
+            this.label18.Text = "Chi nhánh:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtLoginName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(21, 115);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(385, 39);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // txtLoginName
+            // 
+            this.txtLoginName.Location = new System.Drawing.Point(131, 6);
+            this.txtLoginName.Name = "txtLoginName";
+            this.txtLoginName.Size = new System.Drawing.Size(251, 20);
+            this.txtLoginName.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(21, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(385, 39);
+            this.panel2.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(131, 6);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(251, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // btnLocHD
+            // 
+            this.btnLocHD.BackColor = System.Drawing.Color.Transparent;
+            this.btnLocHD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLocHD.Location = new System.Drawing.Point(152, 237);
+            this.btnLocHD.Name = "btnLocHD";
+            this.btnLocHD.Size = new System.Drawing.Size(118, 37);
+            this.btnLocHD.TabIndex = 7;
+            this.btnLocHD.Text = "Đăng nhập";
+            this.btnLocHD.UseVisualStyleBackColor = false;
+            this.btnLocHD.Click += new System.EventHandler(this.btnLocHD_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(288, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 305);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLocHD);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel21);
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panel21.ResumeLayout(false);
+            this.panel21.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel21;
+        private System.Windows.Forms.ComboBox cbChiNhanh;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtLoginName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLocHD;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ QuanLySach/Login.cs
@@ -0,0 +1,45 @@
+﻿using QuanLySach.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySach
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            cbChiNhanh.Items.Add("Chi nhánh 01");
+            cbChiNhanh.Items.Add("Chi nhánh 02");
+
+            cbChiNhanh.SelectedText = "Chi nhánh 01";
+        }
+
+        private void btnLocHD_Click(object sender, EventArgs e)
+        {
+            var userName = txtLoginName.Text;
+            var password = txtPassword.Text;
+            var branch = cbChiNhanh.SelectedText;
+
+            if (branch == "Chi nhánh 01")
+            {
+                DataProvider.Instance.InitConnectionString(App.ChiNhanhEnum.CN_1, userName, password);
+            }
+            else
+            {
+                DataProvider.Instance.InitConnectionString(App.ChiNhanhEnum.CN_2, userName, password);
+            }
+        }
+    }
+}
