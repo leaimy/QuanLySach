4bc0e1f7cc2f7090da2b3ab93b46041542d37b04
Nguyen Trong Hieu <hieuntctk42@gmail.com>
2022-04-07 15:35:11 +0700

    add dto

---
+++ QuanLySach/DTO/SanPhamDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+
+namespace QuanLySach.DTO
+{
+    internal class SanPhamDTO
+    {
+        private int _MaSP;
+        private string _TenSP;
+        private decimal _GiaBan;
+        private int _LoaiSanPham;
+        private string _Mota;
+
+        public int MaSP { get { return _MaSP; } set { _MaSP = value; } }
+        public string TenSP { set { _TenSP = value; } get { return _TenSP; } }
+        public decimal GiaBan { get { return _GiaBan; } set { GiaBan = value; } }
+        public int LoaiSanPham { get { return _LoaiSanPham; } set { LoaiSanPham = value; } }
+        public string Mota { get { return _Mota; } set { _Mota = value; } }
+
+        public SanPhamDTO(int MaSP, string TenSP, decimal GiaBan, int LoaiSanPham, string MoTa)
+        {
+            this.MaSP = MaSP;
+            this.TenSP = TenSP;
+            this.GiaBan = GiaBan;
+            this.LoaiSanPham = LoaiSanPham;
+            this.Mota = Mota;
+        }
+
+        public SanPhamDTO(DataRow row)
+        {
+            this.MaSP = (int)row["MaSP"];
+            this.TenSP = row["TenSP"].ToString();
+            this.GiaBan = (decimal)row["GiaBan"];
+            this.LoaiSanPham = (int)row["LoaiSanPham"];
+            this.Mota = row["Mota"].ToString();
+        }
+    }
+}
